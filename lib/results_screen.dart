2a3319b0cde9eac0e148a6438ce3fc23567b85c7
Marcos Riguetti <marcosriguetti00@hotmail.com>
2023-07-03 11:56:14 -0300

    Projeto de perguntas

---
+++ lib/results_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary/questions_summary.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
+    super.key,
+    required this.choseAnswers,
+    required this.onRestart,
+  });
+
+  final void Function() onRestart;
+  final List<String> choseAnswers;
+
+  List<Map<String, Object>> get summaryData {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choseAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': choseAnswers[i],
+        },
+      );
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final numTotalQuestions = questions.length;
+    final numCorretQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'você respondeu $numCorretQuestions de $numTotalQuestions perguntas corretamente!',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(summaryData),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton.icon(
+              onPressed: onRestart,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.refresh),
+              label: const Text('Reiniciar Quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
